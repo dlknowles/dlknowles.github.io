584e358a8f3f05c5c51282f25d0cda60b84ddc11
dlknowles <dleeknowles@gmail.com>
2023-08-29 08:45:22 -0400

    more tailwindcss stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./**/*.html",
+    "./src/**/*.{html,js,css}"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
 
